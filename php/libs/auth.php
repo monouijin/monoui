dddfbbad2dce703a53fe6d214b1ac63bd1dd006a
NAKANO YUTA <yutasoccer22@ezweb.ne.jp>
2024-11-24 17:16:57 +0900

    ユーザー登録のエラー解消

--- php/libs/auth.php
+++ php/libs/auth.php
@@ -2,9 +2,9 @@
 
 namespace lib;
 
+use db\TopicQuery;
 use db\UserQuery;
 use model\UserModel;
-use lib\Msg;
 use Throwable;
 
 class Auth
@@ -12,10 +12,8 @@ class Auth
     public static function login($id, $pwd)
     {
         try {
-            if (!(
-                UserModel::validateId($id)
-                * UserModel::validatePwd($pwd)
-            )) {
+            if (!(UserModel::validateId($id)
+                * UserModel::validatePwd($pwd))) {
                 return false;
             }
 
@@ -24,19 +22,21 @@ class Auth
             $user = UserQuery::fetchById($id);
 
             if (!empty($user) && $user->del_flg !== 1) {
+
                 if (password_verify($pwd, $user->pwd)) {
                     $is_success = true;
                     UserModel::setSession($user);
                 } else {
-                    Msg::push(Msg::ERROR, 'パスワードが一致しません');
+                    Msg::push(Msg::ERROR, 'パスワードが一致しません。');
                 }
             } else {
-                Msg::push(Msg::ERROR, 'ユーザーが見つかりません');
+                Msg::push(Msg::ERROR, 'ユーザーが見つかりません。');
             }
         } catch (Throwable $e) {
+
             $is_success = false;
             Msg::push(Msg::DEBUG, $e->getMessage());
-            Msg::push(Msg::ERROR, 'ログイン処理でエラーが発生しました');
+            Msg::push(Msg::ERROR, 'ログイン処理でエラーが発生しました。少し時間をおいてから再度お試しください。');
         }
 
         return $is_success;
@@ -56,27 +56,34 @@ class Auth
             $exist_user = UserQuery::fetchById($user->id);
 
             if (!empty($exist_user)) {
-                Msg::push(Msg::ERROR, 'ユーザーがすでに存在します');
+
+                Msg::push(Msg::ERROR, 'ユーザーがすでに存在します。');
                 return false;
             }
 
             $is_success = UserQuery::insert($user);
 
             if ($is_success) {
+
                 UserModel::setSession($user);
             }
         } catch (Throwable $e) {
+
             $is_success = false;
             Msg::push(Msg::DEBUG, $e->getMessage());
-            Msg::push(Msg::ERROR, 'ユーザー登録でエラーが発生しました');
+            Msg::push(Msg::ERROR, 'ユーザー登録でエラーが発生しました。少し時間をおいてから再度お試しください。');
         }
+
+        return $is_success;
     }
 
     public static function isLogin()
     {
         try {
+
             $user = UserModel::getSession();
         } catch (Throwable $e) {
+
             UserModel::clearSession();
             Msg::push(Msg::DEBUG, $e->getMessage());
             return false;
@@ -92,8 +99,10 @@ class Auth
     public static function logout()
     {
         try {
+
             UserModel::clearSession();
         } catch (Throwable $e) {
+
             Msg::push(Msg::DEBUG, $e->getMessage());
             return false;
         }
@@ -104,7 +113,21 @@ class Auth
     public static function requireLogin()
     {
         if (!static::isLogin()) {
-            Msg::push(Msg::ERROR, 'ログインしてください');
+            Msg::push(Msg::ERROR, 'ログインしてください。');
+            redirect('login');
+        }
+    }
+
+    public static function hasPermission($topic_id, $user)
+    {
+        return TopicQuery::isUserOwnTopic($topic_id, $user);
+    }
+
+    public static function requirePermission($topic_id, $user)
+    {
+
+        if (!static::hasPermission($topic_id, $user)) {
+            Msg::push(Msg::ERROR, '編集権限がありません。ログインして再試行してください');
             redirect('login');
         }
     }
--- php/models/user.model.php
+++ php/models/user.model.php
@@ -1,62 +1,74 @@
-<?php
-
+<?php 
 namespace model;
 
 use lib\Msg;
 
-class UserModel extends AbstractModel
-{
+class UserModel extends AbstractModel {
+
     public string $id;
     public string $pwd;
     public string $nickname;
     public int $del_flg;
     public ?string $updated_by;
-    public ?string $created_by;
+    public ?string $created_at;
     public ?string $updated_at;
 
     protected static $SESSION_NAME = '_user';
 
-    public function isValidId()
-    {
+    public function isValidId() {
+
         return static::validateId($this->id);
+        
     }
 
-    public static function validateId($val)
-    {
-
+    public static function validateId($val) {
         $res = true;
-        if (empty($val)) {
-            Msg::push(Msg::ERROR, 'ユーザーIDを入力してください');
+
+        if(empty($val)) {
+
+            Msg::push(Msg::ERROR, 'ユーザーIDを入力してください。');
+            $res = false;
+
         } else {
-            if (strlen($val) > 10) {
-                Msg::push(Msg::ERROR, 'ユーザーIDは10文字以内で入力してください');
+
+            if(strlen($val) > 10) {
+                Msg::push(Msg::ERROR, 'ユーザーIDは１０桁以下で入力してください。');
                 $res = false;
             }
 
-            if (!is_alnum($val)) {
-                Msg::push(Msg::ERROR, 'ユーザーIDは半角英数字入力してください');
+            if(!is_alnum($val)) {
+                Msg::push(Msg::ERROR, 'ユーザーIDは半角英数字で入力してください。');
                 $res = false;
             }
+
         }
 
         return $res;
     }
-
+    
     public static function validatePwd($val)
     {
         $res = true;
 
         if (empty($val)) {
-            Msg::push(Msg::ERROR, 'パスワードを入力してください');
-        } else {
-            if (strlen($val) < 4) {
-                Msg::push(Msg::ERROR, 'パスワードは4桁以上で入力してください。');
-                $res = false;
-            }
 
-            if (!is_alnum($val)) {
-                Msg::push(Msg::ERROR, 'パスワードは半角英数字で入力してください');
+            Msg::push(Msg::ERROR, 'パスワードを入力してください。');
+            $res = false;
+
+        } else {
+
+            if(strlen($val) < 4) {
+
+                Msg::push(Msg::ERROR, 'パスワードは４桁以上で入力してください。');
                 $res = false;
+
+            } 
+            
+            if(!is_alnum($val)) {
+
+                Msg::push(Msg::ERROR, 'パスワードは半角英数字で入力してください。');
+                $res = false;
+
             }
         }
 
@@ -70,17 +82,25 @@ class UserModel extends AbstractModel
 
     public static function validateNickname($val)
     {
+
         $res = true;
 
         if (empty($val)) {
-            Msg::push(Msg::ERROR, "ニックネームを入力してください");
+
+            Msg::push(Msg::ERROR, 'ニックネームを入力してください。');
             $res = false;
+
         } else {
-            if (mb_strlen($val) > 10) {
-                Msg::push(Msg::ERROR, 'ニックネームは10桁以下で入力してください');
+
+            if(mb_strlen($val) > 10) {
+
+                Msg::push(Msg::ERROR, 'ニックネームは１０桁以下で入力してください。');
                 $res = false;
-            }
+                
+            } 
         }
+
+        return $res;
     }
 
     public function isValidNickname()
